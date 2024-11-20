d778268d7ec5150283d2c8638562dc7a157d9761
Kim Hogle <kimberly.senior89@yahoo.com>
2024-11-20 16:00:53 -0500

    Updates, commit 1

--- Form1.cs
+++ Form1.cs
@@ -17,9 +17,39 @@ namespace To_Do_List_App_1
             InitializeComponent();
         }
 
+        DataTable todoList = new DataTable();
+        bool isEditing = false;
+
+
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            // Create Columns
+            todoList.Columns.Add("Title");
+            todoList.Columns.Add("Description");
 
+
+            // Point out datagridview to our datasource
+            toDoListView.DataSource = todoList;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            TitleTextBox.Text = "";
+            DescriptionTextBox.Text = "";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isEditing = true;
+            // Fill text fields with data from table
+            TitleTextBox.Text = todoList.Rows[toDoListView.CurrentCell.RowIndex].ItemArray[0].ToString();
         }
     }
 }
