4b6c486e936bf01943dae0e6176cf645bfc7077b
Kim Hogle <kimberly.senior89@yahoo.com>
2024-12-08 13:26:22 -0500

    Commit 10

---
+++ API/Data/ToDoTasks.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class ToDoTasksContext : DbContext
+    {
+        public ToDoTasksContext(DbContextOptions<ToDoTasksContext> options) : base(options)
+        {
+            
+        }
+    }
+}
